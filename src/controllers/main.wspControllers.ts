80e89afb9eeb7799d82897d90cf6456d179b2c2e
Jose Fuilco <josefuilco@gmail.com>
2023-12-12 15:44:51 -0500

    Third Commit

--- src/controllers/main.wspControllers.ts
+++ src/controllers/main.wspControllers.ts
@@ -1,7 +1,24 @@
-import { Response } from "oak";
+import { Request, Response } from "oak";
 
-export const verifyToken = ({ response }: { response: Response }) => {
-  response.body = "Hola verifyToken";
+export const verifyToken = async ({
+  request,
+  response,
+}: {
+  request: Request;
+  response: Response;
+}) => {
+  try {
+    const accessToken = "RTIIQWWTVHBDSD78S78DSNSND9090DST";
+    const token = request.url.searchParams.get("hub.verify_token");
+    const challenge = (await request.body().value)?.["hub.challenge"];
+
+    if (challenge != null && token != null && token == accessToken) {
+      response.body = challenge;
+    }
+  } catch (error) {
+    response.status = 400;
+    response.body = `${error}`;
+  }
 };
 
 export const receivedMessage = ({ response }: { response: Response }) => {
